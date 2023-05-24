8d5bc34e82601d0094b0ecce623d9185bb2d6634
Jeffry Gonzalez <jeff@hypertheory.com>
2023-05-24 15:26:12 -0400

    acl almost done

--- src/hr/AclSolution/Acl/DataSubscribers.cs
+++ src/hr/AclSolution/Acl/DataSubscribers.cs
@@ -4,18 +4,21 @@ using MessageContracts.JobsApi;
 using JobDomainEvents = MessageContracts.JobsApi;
 using JobOfferingEvents = MessageContracts.JobListingsApi;
 using WebEvents = MessageContracts.WebMessages;
+using AclEvents = MessageContracts.HrAcl;
 namespace Acl;
 
 public class DataSubscribers : ICapSubscribe
 {
 
+    private readonly ICapPublisher _publisher;
     private readonly IDocumentSession _documentSession;
     private readonly ILogger<DataSubscribers> _logger;
 
-    public DataSubscribers(IDocumentSession documentSession, ILogger<DataSubscribers> logger)
+    public DataSubscribers(IDocumentSession documentSession, ILogger<DataSubscribers> logger, ICapPublisher publisher)
     {
         _documentSession = documentSession;
         _logger = logger;
+        _publisher = publisher;
     }
 
     [CapSubscribe("JobsApi.JobCreated")]
@@ -42,5 +45,29 @@ public class DataSubscribers : ICapSubscribe
         await _documentSession.SaveChangesAsync();
     }
 
+    [CapSubscribe("WebMessages.JobApplicationCreated")]
+    public async Task TurnApplicationIntoHiringRequestAsync(WebEvents.JobApplicationCreated application)
+    {
+        // we need the applicant.
+        var applicant = await _documentSession
+            .Query<WebEvents.ApplicantCreated>()
+            .Where(a => a.UserId == application.ApplicantId).SingleAsync();
+        var offer = await _documentSession
+            .Query<JobOfferingEvents.JobListingCreated>()
+            .Where(o => o.Id == application.JobOfferingId).SingleAsync();
 
+        var messageToPublish = new AclEvents.HiringRequestCreated
+        {
+            EmailAddress = applicant.EmailAddress,
+            FirstName = applicant.FirstName,
+            LastName = applicant.LastName,
+            JobId = offer.JobId,
+            OfferingId = application.JobOfferingId
+        };
+
+        await _publisher.PublishAsync(AclEvents.HiringRequestCreated.MessageId, messageToPublish);
+        
+
+    }
 }
+
