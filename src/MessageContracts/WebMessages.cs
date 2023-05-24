eee5dc2db2f8390746a5b20132ed54d8ee48478e
Jeffry Gonzalez <jeff@hypertheory.com>
2023-05-24 11:52:54 -0400

    applicant created

---
+++ src/MessageContracts/WebMessages.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MessageContracts.WebMessages;
+
+public record ApplicantCreated
+{
+    public static readonly string MessageId = "WebMessages.ApplicantCreated";
+    public string UserId { get; set; } = "";
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public DateTimeOffset DateOfBirth { get; set; }
+    public string EmailAddress { get; set; } = "";
+}
+
